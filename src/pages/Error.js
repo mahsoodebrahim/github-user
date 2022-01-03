284e1e86f1959b24773a0659cb35d0927c9dfcc0
Mahsood Ebrahim <mahsoodebrahim@gmail.com>
2022-01-03 15:32:13 -0800

    error page setup

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,7 +1,34 @@
 import React from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 const Error = () => {
-  return <h2>error page</h2>;
+  return (
+    <Wrapper>
+      <div style={{ "text-align": "center" }}>
+        <h1>404</h1>
+        <h3>Hey there, you seem to be lost!</h3>
+        <Link to="/" className="btn">
+          back home
+        </Link>
+      </div>
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.section`
+  min-height: 100vh;
+  display: grid;
+  place-items: center;
+  background: var(--clr-primary-10):
+  text-align: center;
+  h1 {
+    font-size: 10rem
+  }
+  h3 {
+    color: var(--clr-grey-3);
+    margin-bottom: 1.5rem;
+  }
+`;
 
 export default Error;
