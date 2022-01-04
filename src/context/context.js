3a4872b049e463edd753d1d87a2f47d538d37dd9
Mahsood Ebrahim <mahsoodebrahim@gmail.com>
2022-01-04 15:10:17 -0800

    using mock data in context API

--- src/context/context.js
+++ src/context/context.js
@@ -1,10 +1,19 @@
-import React, { createContext, useContext } from "react";
+import React, { useState, createContext, useContext } from "react";
+import mockUser from "./mockData.js/mockUser";
+import mockFollowers from "./mockData.js/mockFollowers";
+import mockRepos from "./mockData.js/mockRepos";
 
 const GithubContext = createContext();
 
 const GithubProvider = ({ children }) => {
+  const [githubUser, setGithubUser] = useState(mockUser);
+  const [repos, setRepos] = useState(mockRepos);
+  const [followers, setFollowers] = useState(mockFollowers);
+
   return (
-    <GithubContext.Provider value="hello">{children}</GithubContext.Provider>
+    <GithubContext.Provider value={{ githubUser, repos, followers }}>
+      {children}
+    </GithubContext.Provider>
   );
 };
 
