dcbf004e3cd6e6165e916b96c979c02ec4259cae
Mahsood Ebrahim <mahsoodebrahim@gmail.com>
2022-01-05 18:12:46 -0800

    fixed centering of User component

--- src/components/User.js
+++ src/components/User.js
@@ -5,13 +5,22 @@ import Followers from "./Followers";
 
 const User = () => {
   return (
-    <Wrapper>
-      <Card />
-      <Followers />
-    </Wrapper>
+    <section className="section">
+      <Wrapper className="section-center">
+        <Card />
+        <Followers />
+      </Wrapper>
+    </section>
   );
 };
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+  padding-top: 2rem;
+  display: grid;
+  gap: 3rem 2rem;
+  @media (min-width: 992px) {
+    grid-template-columns: 1fr 1fr;
+  }
+`;
 
 export default User;
