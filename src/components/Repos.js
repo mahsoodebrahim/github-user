d450cac58d859593f9eafabbd8a336639d73d6f4
Mahsood Ebrahim <mahsoodebrahim@gmail.com>
2022-01-05 19:00:46 -0800

    dynamically create example chart

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -1,4 +1,5 @@
 import React from "react";
+import styled from "styled-components";
 import { useGlobalGithubContext } from "../context/context";
 import { Bar3D, Column3D, Doughnut2D, ExampleChart, Pie3D } from "./Charts";
 
@@ -6,12 +7,32 @@ const Repos = () => {
   const { repos } = useGlobalGithubContext();
   console.log(repos);
 
+  const chartData = [
+    {
+      label: "HTML",
+      value: "13",
+    },
+    {
+      label: "CSS",
+      value: "23",
+    },
+    {
+      label: "JavaScript",
+      value: "80",
+    },
+  ];
+
   return (
-    <div>
-      <h2>repos</h2>
-      <ExampleChart />
-    </div>
+    <section className="section">
+      <Wrapper className="section-center">
+        <div>
+          <ExampleChart data={chartData} />
+        </div>
+      </Wrapper>
+    </section>
   );
 };
+
+const Wrapper = styled.div``;
 
 export default Repos;
