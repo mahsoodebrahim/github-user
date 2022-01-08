b57c563e458cb732e6da45b007b46338ebbdff1a
Mahsood Ebrahim <mahsoodebrahim@gmail.com>
2022-01-08 11:38:40 -0800

    add bar and column charts for most forked and popular repositories, respectively

--- src/components/Charts/Bar3D.js
+++ src/components/Charts/Bar3D.js
@@ -1,7 +1,33 @@
 import React from "react";
+import ReactFC from "react-fusioncharts";
+import FusionCharts from "fusioncharts";
+import Charts from "fusioncharts/fusioncharts.charts";
+import UmberTheme from "fusioncharts/themes/fusioncharts.theme.umber";
 
-const Bar3D = () => {
-  return <h2>bar3d</h2>;
+ReactFC.fcRoot(FusionCharts, Charts, UmberTheme);
+
+const ChartComponent = ({ data }) => {
+  const chartConfigs = {
+    type: "bar3d", // The chart type
+    width: "100%", // Width of the chart
+    height: "400", // Height of the chart
+    dataFormat: "json", // Data type
+    dataSource: {
+      // Chart Configuration
+      chart: {
+        caption: "Popular Forks",
+        theme: "umber",
+        xAxisName: "Forks",
+        yAxisName: "Repos",
+        xAxisNameFontSize: "16px",
+        yAxisNameFontSize: "16px",
+      },
+      // Chart Data
+      data,
+    },
+  };
+
+  return <ReactFC {...chartConfigs} />;
 };
 
-export default Bar3D;
+export default ChartComponent;
--- src/components/Charts/Column3D.js
+++ src/components/Charts/Column3D.js
@@ -1,7 +1,33 @@
 import React from "react";
+import ReactFC from "react-fusioncharts";
+import FusionCharts from "fusioncharts";
+import Charts from "fusioncharts/fusioncharts.charts";
+import CandyTheme from "fusioncharts/themes/fusioncharts.theme.candy";
 
-const Column3D = () => {
-  return <h2>column3d</h2>;
+ReactFC.fcRoot(FusionCharts, Charts, CandyTheme);
+
+const ChartComponent = ({ data }) => {
+  const chartConfigs = {
+    type: "column3d", // The chart type
+    width: "100%", // Width of the chart
+    height: "400", // Height of the chart
+    dataFormat: "json", // Data type
+    dataSource: {
+      // Chart Configuration
+      chart: {
+        caption: "Popular Repositories",
+        theme: "candy",
+        xAxisName: "Repos",
+        yAxisName: "Stars",
+        xAxisNameFontSize: "16px",
+        yAxisNameFontSize: "16px",
+      },
+      // Chart Data
+      data,
+    },
+  };
+
+  return <ReactFC {...chartConfigs} />;
 };
 
-export default Column3D;
+export default ChartComponent;
