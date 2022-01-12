1bfdae94479890b44cd46911327183b403ad984f
Mahsood Ebrahim <mahsoodebrahim@gmail.com>
2022-01-12 15:20:25 -0800

    setting up login-logout using Auth0 API

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,70 @@
 import React from "react";
+import styled from "styled-components";
+import { useAuth0 } from "@auth0/auth0-react";
 
 const Navbar = () => {
-  return <h2>navbar component</h2>;
+  const { user, isAuthenticated, loginWithRedirect, logout, isLoading } =
+    useAuth0();
+
+  console.log(user, isAuthenticated, isLoading);
+
+  if (isLoading) {
+    return <div>Loading ...</div>;
+  }
+
+  return (
+    <Wrapper>
+      {isAuthenticated ? (
+        <>
+          <img src={user.picture} alt="" />
+          <h4>
+            Welcome, <strong>{user.name.toUpperCase()}</strong>
+          </h4>
+          <button onClick={() => logout({ returnTo: window.location.origin })}>
+            log out
+          </button>
+        </>
+      ) : (
+        <button onClick={() => loginWithRedirect()}>log in</button>
+      )}
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.nav`
+  padding: 1.5rem;
+  margin-bottom: 4rem;
+  background: var(--clr-white);
+  text-align: center;
+  display: grid;
+  grid-template-columns: auto auto 100px;
+  justify-content: center;
+  align-items: center;
+  gap: 1.5rem;
+
+  img {
+    width: 35px;
+    height: 35px;
+    border-radius: 50%;
+    object-fit: cover;
+  }
+
+  h4 {
+    font-weight: 400;
+    margin-bottom: 0;
+  }
+
+  button {
+    border: transparent;
+    background: transparent;
+    color: var(--clr-grey-5);
+    margin-left: 1rem;
+    font-size: 1.2rem;
+    border-radius: 10px;
+    text-transform: capitalize;
+    letter-spacing: var(--spacing);
+    cursor: pointer;
+  }
+`;
 
 export default Navbar;
--- src/index.js
+++ src/index.js
@@ -3,12 +3,19 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { GithubProvider } from "./context/context";
+import { Auth0Provider } from "@auth0/auth0-react";
 
 ReactDOM.render(
   <React.StrictMode>
-    <GithubProvider>
-      <App />
-    </GithubProvider>
+    <Auth0Provider
+      domain="dev-b4y3u64o.us.auth0.com"
+      clientId="dlthHxQLjIrcfDHmZIFtdVs5FULxEyEy"
+      redirectUri={window.location.origin}
+    >
+      <GithubProvider>
+        <App />
+      </GithubProvider>
+    </Auth0Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
